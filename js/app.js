007b0198fd89c5f1345ac6fbc6f9c574890b217a
gh-unicorn <inmatrixos@gmail.com>
2018-03-02 14:30:15 +0100

    get JSON

--- js/app.js
+++ js/app.js
@@ -26,36 +26,3 @@ $(document).ready(function(){
 };
 };
 });
-
-
-
-
-
-// $(document).ready(function(){
-//   $.ajax({
-//    url:"http://api.openweathermap.org/data/2.5/weather?q=London&APPID=14e151e484d9f117a91b94f3d9b10501&id=5128581&units=imperial",
-//     dataType: 'json',
-//     success: function(data) {
-//     var fToCel = ((10*Math.round(data.main.temp - 32) * 5 / 9))/10 +"&ordm;"+" Celsius";
-//    $('#btnLondon').one( "click", function() {
-//     var newDiv = $('<div class="listing listing_ad job"></div>');
-//   $('body').append(newDiv);
-//   var temper = data.rates[0].mid;;
-// var round_temp = Math.floor(temper);
-// var fToCel_1 = ((round_temp-32)*5);
-// var fToCel_2 = fToCel_1/9;
-// var fToCel = Math.floor(fToCel_2);
-//  $('<p class = "pClass">'+fToCel+'</p>').appendTo('#subtitle');
-//  $('#btnReset').one( "click", function() {
-//   var remov = $("#subtitle").remove();
-// });
-// });
-//     },
-//     error: function() {
-//       alert("error");
-//     }
-//   });   
-//   $('.btn').click(function() {
-//     location.reload();
-// });     
-// });
